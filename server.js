68974b9bd7f4d049ba16cf6dbf5497291cd7bbf4
GuruVedhaRamanan <30861666+GuruVedhaRamanan@users.noreply.github.com>
2017-08-11 22:51:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,16 +81,16 @@ var htmltemplate= `
 app.get('/', function (req, res)  {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+ });
 app.get('/:articleName',function(req,res)
 {
      var articleName =req.params.articleName;
      res.send(createtemplate(articles[articleName]));
 
 });
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
- });
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
