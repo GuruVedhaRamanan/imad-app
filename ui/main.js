46fefb060c3d29b5ce138a96093e9adeae03ed2a
GuruVedhaRamanan <30861666+GuruVedhaRamanan@users.noreply.github.com>
2017-08-11 23:01:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,14 +3,7 @@ var element = document.getElementById("maintext");
 element.innerHTML = "Newvalue";
 //to move the image 
 var img = document.getElementById("guru");
-var marginLeft= 0;
-function moveRight(){
-    marginLeft = marginLeft+1;
-    img.style.marginLeft = marginLeft+'px';
-}
-img.onclick = function()
+img.onclick =function()
 {
-    var interval = setinterval(moveRight,50);
-}; 
-
-
+    img.style.marginleft = '100px';
+};
