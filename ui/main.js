449e0fcb13d14c18507f311dc17cf1975a26c0b1
GuruVedhaRamanan <30861666+GuruVedhaRamanan@users.noreply.github.com>
2017-08-11 20:40:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
 
 //changin to the text to new value
-var element=document.getElementByID('main-text');
-element.innerHTML('Newvalue');
+var element=document.getElementByID('main-text')
+element.innerHTML='Newvalue'
