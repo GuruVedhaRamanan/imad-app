3154d62f041e42650b187c7aef997c3f93a58d51
GuruVedhaRamanan <30861666+GuruVedhaRamanan@users.noreply.github.com>
2017-08-12 12:53:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,23 @@
-console.log('Loaded!');
-var element = document.getElementById("maintext");
-element.innerHTML = "GURU'S PRACTICAL WORK";
-//to move the image 
-var img = document.getElementById("guru");
-var marginLeft= 0;
-function moveRight(){
-    marginLeft = marginLeft+10;
-    img.style.marginLeft = marginLeft+'px';
-}
-img.onclick = function()
+var button=document.getElementById("counter");
+button.onclick=function()
 {
-    var interval = setInterval(moveRight,50);
-}; 
-
-
-
+    //make a request object
+    var request=new XMLHTTPRequest();
+    // capturing the response and store it in a variable
+    request.onreadystatechange = function()
+    {
+        if(request.readystate===XMLHTTPRequest.Done)
+        {//if the request is done we want to do some activities
+            if(request.status ===200)
+            {
+                var counter=request.responseText;
+                var span=document.getElementById('count');
+                span.InnerHTML = counter.toString();
+                
+            }
+        }
+    };
+    //make the request
+    request.open('GET','http://guruvedharamanan20cs.imad.hasura-app.io/counter',true);
+    request.send(null);
+};
