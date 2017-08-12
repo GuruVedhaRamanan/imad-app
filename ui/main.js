c45ad012396b9948c0d3a88b62e58fe6454b6c9b
GuruVedhaRamanan <30861666+GuruVedhaRamanan@users.noreply.github.com>
2017-08-12 13:15:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,7 +9,6 @@ button.onclick=function()
              if(request.status==200)
              {
                  var counter=request.responseText;
-                 
                 var span=document.getElementById("count");
                 span.innerHTML = counter.toString();
              }
