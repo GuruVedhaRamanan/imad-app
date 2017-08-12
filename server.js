fa8e9e884bd2372f086485175aed1649eaa258ea
GuruVedhaRamanan <30861666+GuruVedhaRamanan@users.noreply.github.com>
2017-08-12 14:12:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -106,8 +106,8 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 var names= [];
-app.get('/submit-name/:name',function(req,res){
-   var name =req.params.name;
+app.get('/submit-name',function(req,res){
+   var name =req.query.name;
    names.push(name);
 // JSON -java script obi=ject notation
    res.send(JSON.stringify(names));
