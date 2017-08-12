e2dc7f469814d22ebc50cb30e5fb343e7c7935c5
GuruVedhaRamanan <30861666+GuruVedhaRamanan@users.noreply.github.com>
2017-08-12 17:55:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,10 +30,7 @@ button.onclick=function()
                  var names = request.responseText;
                  names=JSON.parse(names);
                  var list =' '; 
-              for (var i=0;i<names.length;i++)
-              {
-                  list+= '<li>' + names[i] +'</li>';
-              }
+            
              var ul = document.getElementById("namelist");
              ul.innerHTML = list;
              }
@@ -44,7 +41,8 @@ button.onclick=function()
      request.open('GET','http://guruvedharamanan20cs.imad.hasura-app.io/submit-name/?name='+name,true);
      request.send(null);
    };
-             
+            
+                              
         
   
    
