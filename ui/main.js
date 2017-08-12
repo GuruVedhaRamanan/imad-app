b46b6ac742ab4b7890c95a8239fd661140f7cbe7
GuruVedhaRamanan <30861666+GuruVedhaRamanan@users.noreply.github.com>
2017-08-12 13:14:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,23 @@
 var button=document.getElementById("counter");
-var counter=0;
 button.onclick=function()
-{
-                  counter=counter+1;
-                  
+{  
+    var request = new XMLHTTPRequest();
+     request.onreadystatechange = function()
+     {
+         if(request.readystate===XMLHTTPRequest.DONE)
+         {
+             if(request.status==200)
+             {
+                 var counter=request.responseText;
+                 
                 var span=document.getElementById("count");
                 span.innerHTML = counter.toString();
-};
+             }
+         }
+     };
+     request.open("GET","http://guruvedharamanan20cs.imad.hasura-app.io/counter",true);
+     request.send(null);
+    };
+      
+                
    
