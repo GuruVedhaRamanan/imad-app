0bc2f8b50e2fea7880f34db7c9fa1bb68372c22b
GuruVedhaRamanan <30861666+GuruVedhaRamanan@users.noreply.github.com>
2017-08-12 19:12:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,30 +16,7 @@ button.onclick=function()
      };
      request.open('GET','http://guruvedharamanan20cs.imad.hasura-app.io/counter',true);
      request.send(null);
-    };
-      
-    var submit =document.getElementById("submit_btn");
-   submit.onclick = function(){
-     var request = new XMLHttpRequest();
-     request.onreadystatechange = function()
-     {
-         if(request.readyState == XMLHttpRequest.DONE)
-         {
-             if(request.status ==200)
-             {
-                 var names = request.responseText;
-                 names=JSON.parse(names);
-                var ul = document.getElementById("namelist");
-             ul.innerHTML = names;
-             }
-         }
-     };
-      var nameInput=document.getElementById("name");
-      var name = nameInput.value;
-     request.open('GET','http://guruvedharamanan20cs.imad.hasura-app.io/submit-name/?name='+name,true);
-     request.send(null);
-   }; 
-   var comment =document.getElementById("comment_btn");
+    }; var comment =document.getElementById("comment_btn");
    comment.onclick = function(){
      var request = new XMLHttpRequest();
      request.onreadystatechange = function()
@@ -60,4 +37,25 @@ button.onclick=function()
      request.open('GET','http://guruvedharamanan20cs.imad.hasura-app.io/comment/?comment='+comment,true);
      request.send(null);
    };
-       
+       var submit =document.getElementById("submit_btn");
+   submit.onclick = function(){
+     var request = new XMLHttpRequest();
+     request.onreadystatechange = function()
+     {
+         if(request.readyState == XMLHttpRequest.DONE)
+         {
+             if(request.status ==200)
+             {
+                 var names = request.responseText;
+                 names=JSON.parse(names);
+                var ul = document.getElementById("namelist");
+             ul.innerHTML = names;
+             }
+         }
+     };
+      var nameInput=document.getElementById("name");
+      var name = nameInput.value;
+     request.open('GET','http://guruvedharamanan20cs.imad.hasura-app.io/submit-name/?name='+name,true);
+     request.send(null);
+   }; 
+  
