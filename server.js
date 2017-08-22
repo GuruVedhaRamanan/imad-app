3ed816112252676215262dc4381edb003a04f4e1
GuruVedhaRamanan <30861666+GuruVedhaRamanan@users.noreply.github.com>
2017-08-22 13:16:42 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -127,7 +127,7 @@ app.get('/commend',function(req,res){
 });
 app.get('/article/:articleName',function(req,res){
    
-    pool.query("SELECT * FROM article WHERE title='" + req.params.articleName +"'",function(err,result){
+    pool.query("SELECT * FROM article WHERE title="+ req.params.articleName,function(err,result){
         if(err)
         {
             res.status(500).send(err.toString());
@@ -142,7 +142,7 @@ app.get('/article/:articleName',function(req,res){
         else
         {
             var articleData = result.row[0];
-         res.send(createtemplate(articledata));
+         res.send(createtemplate(articleData));
         }
         }
 });  
