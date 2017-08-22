bdb8322e086fc8e60c47630a1a97c0e674fba389
GuruVedhaRamanan <30861666+GuruVedhaRamanan@users.noreply.github.com>
2017-08-22 11:57:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
 
+var config={
+user:'guruvedharamanan20cs',
+database:'guruvedharamanan20cs',
+host:'db.imad.hasura-app.io',
+port:'5432',
+password:process.env.DB_PASSWORD,
+};
 var app = express();
 app.use(morgan('combined'));
 var articles ={
@@ -83,6 +91,21 @@ app.get('/', function (req, res)  {
 });
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+ });
+ var pool =new Pool(config);
+ app.get('/test-db',function(req,res){
+    
+     //make a select to database
+     pool.query('SELECT * FROM test',function(err,result){
+     if(err)
+     {
+         res.status(500).send(err.toString());
+     }
+     else
+     {
+         res.send(JSON.stringify(result));
+     }
+ });
  });
  var counter=0;
  app.get('/counter',function(req,res)
