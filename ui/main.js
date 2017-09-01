da5b97d647db176765ccda78a1f453f5741c4f9a
GuruVedhaRamanan <30861666+GuruVedhaRamanan@users.noreply.github.com>
2017-09-01 13:16:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,3 +30,24 @@
      request.send(JSON.stringify({ username: username,password: password}));
    }; 
   
+  var register =document.getElementById("register_btn");
+register.onclick = function(){
+     var request = new XMLHttpRequest();
+     request.onreadystatechange = function()
+     {
+         if(request.readyState == XMLHttpRequest.DONE)
+         {
+             if(request.status ==200)
+             {
+                 console.log("logged in succefully");
+                 alert("you have suceefully registered");
+             }
+         }   
+     };
+      var username=document.getElementById("username").value;
+      var password=document.getElementById("password").value;
+     request.open('POST','http://guruvedharamanan20cs.imad.hasura-app.io/create-user',true);
+     request.setRequestHeader('Content-Type','application/json');
+     request.send(JSON.stringify({ username: username,password: password}));
+   }; 
+  
